ceea1771c71239d5d81943f2307f9054cfa8eebb
u101040 <benbenkatz@gmail.com>
2023-04-28 13:00:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chess/Piece.java
@@ -0,0 +1,25 @@
+package com.example.chess;
+
+public class Piece {
+    private int i;
+    private int j;
+    public Piece(int i, int j){
+        this.i = i;
+        this.j = j;
+    }
+    public int GetI(){
+        return this.i;
+    }
+    public int GetJ(){
+        return this.j;
+    }
+    public void SetI(int i){
+        this.i = i;
+    }
+    public boolean HasPiece(){
+        return false;
+    }
+    public boolean CanMove(int i, int j){
+        return false;
+    }
+}
